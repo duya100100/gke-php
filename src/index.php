f179e460fea945ee56adfb42c59d36afa8508b60
duya100100 <55070573+duya100100@users.noreply.github.com>
2019-10-29 14:14:28 +0700

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,11 +1,9 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Kubernetes Php Example</title>
-</head>
 <body>
-<p>
-    Hello World. Today is <?= date('l \t\h\e jS') ?>.
-</p>
+
+<h1>Hello GKE!</h1>
+<p>Hello there from IP <?php echo $_SERVER['SERVER_ADDR']; ?> </p>
+
 </body>
 </html>
